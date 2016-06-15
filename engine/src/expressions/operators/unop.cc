65c6b2ad3915817342f762e9d44abd626e2e81f1
Ariane van der Steldt <ariane@stack.nl>
2016-06-15 23:34:02 +0100

    Implement abstract base for unary operators.

---
+++ engine/src/expressions/operators/unop.cc
@@ -0,0 +1,34 @@
+#include <monsoon/expressions/operators/unop.h>
+#include <stdexcept>
+#include <utility>
+
+namespace monsoon {
+namespace expressions {
+namespace operators {
+
+
+unop::unop(const char* symbol, std::unique_ptr<expression> x)
+: symbol_(symbol),
+  x_(std::move(x))
+{
+  if (x_ == nullptr)
+    throw std::invalid_argument("nullptr child expression");
+}
+
+unop::~unop() noexcept {}
+
+auto unop::config_string() const -> std::string {
+  return symbol_ + x_->config_string();
+}
+
+auto unop::evaluate(const context& ctx) const
+->  std::unordered_map<tags, metric_value> {
+  std::unordered_map<tags, metric_value> result = x_->evaluate(ctx);
+
+  for (auto& x_pair : result)
+    x_pair.second = evaluate(x_pair.second);
+  return result;
+}
+
+
+}}} /* namespace monsoon::expressions::operators */
