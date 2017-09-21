6b9158e57040d946ad23dd7d22bc4733e73dec02
Ariane van der Steldt <ariane@stack.nl>
2017-09-21 15:38:47 +0100

    Implement read/write.

--- lib/misc/include/monsoon/fd.h
+++ lib/misc/include/monsoon/fd.h
@@ -1,14 +1,24 @@
 #ifndef FD_H
 #define FD_H
 
+#include <cstddef>
 #include <string>
 #include <monsoon/optional.h>
+
+#if defined(WIN32)
+# include <WinNT.h>
+#endif
 
 namespace monsoon {
 
 
 class fd {
  public:
+#if defined(WIN32)
+  using implementation_type = HANDLE;
+#else
+  using implementation_type = int;
+#endif
   enum open_mode {
     READ_ONLY,
     WRITE_ONLY,
@@ -28,6 +38,7 @@ class fd {
 
   void close();
   explicit operator bool() const noexcept;
+  bool is_open() const noexcept { return static_cast<bool>(*this); }
   bool can_read() const noexcept;
   bool can_write() const noexcept;
   offset_type offset() const;
@@ -36,13 +47,20 @@ class fd {
   void flush();
   size_type size() const;
 
+  std::size_t read(void*, std::size_t);
+  std::size_t write(const void*, std::size_t);
+  std::size_t read_at(offset_type, void*, std::size_t);
+  std::size_t write_at(offset_type, const void*, std::size_t);
+
   void swap(fd&) noexcept;
+
+  implementation_type underlying() const noexcept { return handle_; }
 
  private:
 #if defined(WIN32)
-  HANDLE handle_;
+  implementation_type handle_;
 #else
-  int fd_;
+  implementation_type handle_;
   std::string fname_;
 #endif
 
--- lib/misc/src/fd.cc
+++ lib/misc/src/fd.cc
@@ -3,6 +3,8 @@
 #if defined(WIN32)
 
 #include <Windows.h>
+#include <limits>
+#include <type_traits>
 #include <utility>
 #include <vector>
 
@@ -51,7 +53,7 @@ fd::fd(const std::string& fname, open_mode mode)
       FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
       nullptr,
       OPEN_EXISTING,
-      FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED,
+      FILE_ATTRIBUTE_NORMAL,
       nullptr);
   if (handle_ == INVALID_HANDLE_VALUE) throw_last_error_();
 }
@@ -116,6 +118,59 @@ auto fd::size() const -> size_type {
   return v;
 }
 
+auto fd::read(void* buf, std::size_t nbytes) -> std::size_t {
+  if (nbytes > std::numeric_limits<DWORD>::max())
+    nbytes = std::numeric_limits<DWORD>::max();
+
+  DWORD rlen;
+  if (!ReadFile(handle_, buf, nbytes, &rlen, nullptr)) throw_last_error_();
+  return rlen;
+}
+
+auto fd::write(const void* buf, std::size_t nbytes) -> std::size_t {
+  if (nbytes > std::numeric_limits<DWORD>::max())
+    nbytes = std::numeric_limits<DWORD>::max();
+
+  DWORD wlen;
+  if (!WriteFile(handle_, buf, nbytes, &wlen, nullptr)) throw_last_error_();
+  return wlen;
+}
+
+inline OVERLAPPED make_overlapped(fd::offset_type off) {
+  using u_DWORD = std::make_unsigned_t<DWORD>;
+  using u_DWORD_lims = std::numeric_limits<u_DWORD>;
+
+  OVERLAPPED overlapped;
+  overlapped.Internal = nullptr;
+  overlapped.InternalHigh = nullptr;
+  overlapped.Offset = static_cast<u_DWORD>(off);
+  overlapped.OffsetHigh = static_cast<u_DWORD>(off >> u_DWORD_lims::digits);
+  overlapped.hEvent = INVALID_HANDLE_VALUE;
+  return overlapped;
+}
+
+auto fd::read_at(offset_type off, void* buf, std::size_t nbytes)
+-> std::size_t {
+  if (nbytes > std::numeric_limits<DWORD>::max())
+    nbytes = std::numeric_limits<DWORD>::max();
+  OVERLAPPED ol_data = make_overlapped(off);
+
+  DWORD rlen;
+  if (!ReadFile(handle_, buf, nbytes, &rlen, &ol_data)) throw_last_error_();
+  return rlen;
+}
+
+auto fd::write_at(offset_type off, const void* buf, std::size_t nbytes)
+-> std::size_t {
+  if (nbytes > std::numeric_limits<DWORD>::max())
+    nbytes = std::numeric_limits<DWORD>::max();
+  OVERLAPPED ol_data = make_overlapped(off);
+
+  DWORD wlen;
+  if (!WriteFile(handle_, buf, nbytes, &wlen, &ol_data)) throw_last_error_();
+  return wlen;
+}
+
 void fd::swap(fd& o) noexcept {
   std::swap(handle_, o.handle_);
   std::swap(mode_, o.mode_);
@@ -145,15 +200,15 @@ static void throw_errno_() {
 } /* namespace monsoon::<anonymous> */
 
 fd::fd() noexcept
-: fd_(-1)
+: handle_(-1)
 {}
 
 fd::fd(fd&& o) noexcept
-: fd_(std::exchange(o.fd_, -1))
+: handle_(std::exchange(o.handle_, -1))
 {}
 
 fd::fd(const std::string& fname, open_mode mode)
-: fd_(-1),
+: handle_(-1),
   fname_(normalize(fname)),
   mode_(mode)
 {
@@ -170,8 +225,8 @@ fd::fd(const std::string& fname, open_mode mode)
       break;
   }
 
-  fd_ = ::open(fname_.c_str(), fl);
-  if (fd_ == -1) throw_errno_();
+  handle_ = ::open(fname_.c_str(), fl);
+  if (handle_ == -1) throw_errno_();
 }
 
 fd::~fd() noexcept {
@@ -179,42 +234,68 @@ fd::~fd() noexcept {
 }
 
 void fd::close() {
-  if (fd_ != -1) {
-    ::close(fd_);
-    fd_ = -1;
+  if (handle_ != -1) {
+    ::close(handle_);
+    handle_ = -1;
   }
 }
 
 fd::operator bool() const noexcept {
-  return fd_ != -1;
+  return handle_ != -1;
 }
 
 auto fd::offset() const -> offset_type {
-  const auto off = lseek(fd_, 0, SEEK_CUR);
+  const auto off = lseek(handle_, 0, SEEK_CUR);
   if (off == -1) throw_errno_();
   return off;
 }
 
 optional<std::string> fd::get_path() const {
-  if (fd_ == -1 || fname_.empty())
+  if (handle_ == -1 || fname_.empty())
     return {};
   return fname_;
 }
 
 void fd::flush() {
-  if (fsync(fd_)) throw_errno_();
+  if (fsync(handle_)) throw_errno_();
 }
 
 auto fd::size() const -> size_type {
   struct stat sb;
 
-  if (fstat(fd_, &sb)) throw_errno_();
+  if (fstat(handle_, &sb)) throw_errno_();
   return sb.st_size;
+}
+
+auto fd::read(void* buf, std::size_t nbytes) -> std::size_t {
+  const auto rlen = ::read(handle_, buf, nbytes);
+  if (rlen == -1) throw_errno_();
+  return rlen;
+}
+
+auto fd::write(const void* buf, std::size_t nbytes) -> std::size_t {
+  const auto rlen = ::write(handle_, buf, nbytes);
+  if (rlen == -1) throw_errno_();
+  return rlen;
+}
+
+auto fd::read_at(offset_type off, void* buf, std::size_t nbytes)
+-> std::size_t {
+  const auto rlen = ::pread(handle_, buf, nbytes, off);
+  if (rlen == -1) throw_errno_();
+  return rlen;
+}
+
+auto fd::write_at(offset_type off, const void* buf, std::size_t nbytes)
+-> std::size_t {
+  const auto rlen = ::pwrite(handle_, buf, nbytes, off);
+  if (rlen == -1) throw_errno_();
+  return rlen;
 }
 
 void fd::swap(fd& o) noexcept {
   std::swap(fname_, o.fname_);
-  std::swap(fd_, o.fd_);
+  std::swap(handle_, o.handle_);
   std::swap(mode_, o.mode_);
 }
 
