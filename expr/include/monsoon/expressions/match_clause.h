a3d899e21415fbcd794cef6fff805f7e1eb6f8f7
Ariane van der Steldt <ariane@stack.nl>
2017-12-31 20:00:49 +0000

    Implement match clauses.

---
+++ expr/include/monsoon/expressions/match_clause.h
@@ -0,0 +1,120 @@
+#ifndef MONSOON_EXPRESSIONS_MATCH_CLAUSE_H
+#define MONSOON_EXPRESSIONS_MATCH_CLAUSE_H
+
+///\file
+///\brief A match clause for vector operations.
+///\ingroup expr
+
+#include <monsoon/expr_export_.h>
+#include <monsoon/tags.h>
+#include <string>
+#include <vector>
+#include <unordered_set>
+#include <type_traits>
+
+namespace monsoon {
+namespace expressions {
+
+
+enum class match_clause_keep {
+  none,
+  left,
+  right,
+  common
+};
+
+/**
+ * \brief A match clause.
+ * \ingroup expr
+ *
+ * Match clauses test if a pair of \ref monsoon::tags "tag sets" match.
+ * Matched tag sets are used to match values together in binary operations.
+ */
+class monsoon_expr_export_ match_clause {
+ public:
+  ///\brief Destructor.
+  virtual ~match_clause() noexcept;
+
+  /**
+   * \brief Check if the \ref monsoon::tags "tag set" has correct tags.
+   * This predicate checks if the tag names in \p x
+   * are the ones this match clause requires.
+   *
+   * If this function returns false, the tags shall not be passed
+   * to other match clause functions.
+   * \return True if the tag names in \p x are sufficient
+   * for the match clause to operate on.
+   */
+  virtual bool pass(const tags& x) const noexcept = 0;
+
+  /**
+   * \brief Less comparison on two tags.
+   * Yields true if x comes before y,
+   * taking into account the configuration of the match clause.
+   * \return True if x is ordered before y.
+   */
+  virtual bool less_cmp(const tags& x, const tags& y) const noexcept = 0;
+
+  /**
+   * \brief Reduces tag set.
+   * \return A merged tag set, constructed from the two input tag sets.
+   */
+  virtual tags reduce(const tags& x, const tags& y) const = 0;
+};
+
+class monsoon_expr_export_ by_match_clause
+: public match_clause
+{
+ public:
+  template<typename Iter>
+  by_match_clause(
+      Iter b, Iter e,
+      match_clause_keep keep = match_clause_keep::none)
+  : tag_names_(b, e),
+    keep_(keep)
+  {
+    fixup_();
+  }
+
+  ~by_match_clause() noexcept override;
+
+  ///\copydoc match_clause::pass(const tags&);
+  bool pass(const tags& x) const noexcept override;
+  ///\copydoc match_clause::less_cmp(const tags&, const tags&);
+  bool less_cmp(const tags& x, const tags& y) const noexcept override;
+  ///\copydoc match_clause::reduce(const tags&, const tags&);
+  tags reduce(const tags& x, const tags& y) const override;
+
+ private:
+  void fixup_() noexcept;
+
+  std::vector<std::string> tag_names_; // Sorted vector.
+  match_clause_keep keep_ = match_clause_keep::none;
+};
+
+class monsoon_expr_export_ without_match_clause
+: public match_clause
+{
+ public:
+  template<typename Iter>
+  without_match_clause(Iter b, Iter e)
+  : tag_names_(b, e)
+  {}
+
+  ~without_match_clause() noexcept override;
+
+  ///\copydoc match_clause::pass(const tags&);
+  bool pass(const tags& x) const noexcept override;
+  ///\copydoc match_clause::less_cmp(const tags&, const tags&);
+  bool less_cmp(const tags& x, const tags& y) const noexcept override;
+  ///\copydoc match_clause::reduce(const tags&, const tags&);
+  tags reduce(const tags& x, const tags& y) const override;
+
+ private:
+  std::unordered_set<std::string> tag_names_;
+};
+
+
+}} /* namespace monsoon::expressions */
+
+#endif /* MONSOON_EXPRESSIONS_MATCH_CLAUSE_H */
---
+++ expr/src/expressions/match_clause.cc
@@ -0,0 +1,136 @@
+#include <monsoon/expressions/match_clause.h>
+#include <algorithm>
+#include <cassert>
+#include <iterator>
+#include <monsoon/overload.h>
+
+namespace monsoon {
+namespace expressions {
+
+
+match_clause::~match_clause() noexcept {}
+
+
+by_match_clause::~by_match_clause() noexcept {}
+
+bool by_match_clause::pass(const tags& x) const noexcept {
+  return x.has_keys(tag_names_.begin(), tag_names_.end());
+}
+
+bool by_match_clause::less_cmp(const tags& x, const tags& y) const noexcept {
+  for (const auto& name : tag_names_) {
+    const std::optional<metric_value> x_val = x[name];
+    const std::optional<metric_value> y_val = y[name];
+    assert(x_val.has_value() && y_val.has_value());
+
+    if (metric_value::before(*x_val, *y_val)) return true;
+    if (metric_value::before(*y_val, *x_val)) return false;
+  }
+  return false;
+}
+
+tags by_match_clause::reduce(const tags& x, const tags& y) const {
+  tags::map_type result;
+
+  switch (keep_) {
+    case match_clause_keep::none:
+      std::set_intersection(
+          x.begin(), x.end(),
+          tag_names_.begin(), tag_names_.end(),
+          std::inserter(result, result.end()),
+          overload(
+              [](const std::pair<const std::string, metric_value>& x, const std::string& y) {
+                return x.first < y;
+              },
+              [](const std::string x, const std::pair<const std::string, metric_value>& y) {
+                return x < y.first;
+              }));
+      break;
+    case match_clause_keep::left:
+      result = x.get_map();
+      break;
+    case match_clause_keep::right:
+      result = y.get_map();
+      break;
+    case match_clause_keep::common:
+      std::set_intersection(
+          x.begin(), x.end(),
+          y.begin(), y.end(),
+          std::inserter(result, result.end()),
+          [](const auto& x, const auto& y) {
+            if (std::get<0>(x) != std::get<0>(y))
+              return std::get<0>(x) < std::get<0>(y);
+            return metric_value::before(x.second, y.second);
+          });
+      break;
+  }
+
+  return tags(std::move(result));
+}
+
+void by_match_clause::fixup_() noexcept {
+  std::sort(tag_names_.begin(), tag_names_.end());
+  tag_names_.erase(
+      std::unique(tag_names_.begin(), tag_names_.end()),
+      tag_names_.end());
+  tag_names_.shrink_to_fit();
+}
+
+
+without_match_clause::~without_match_clause() noexcept {}
+
+bool without_match_clause::pass(const tags& x) const noexcept {
+  return true;
+}
+
+bool without_match_clause::less_cmp(const tags& x, const tags& y)
+    const noexcept {
+  auto x_i = x.begin(), y_i = y.begin();
+  const auto x_end = x.end(), y_end = y.end();
+
+  while (x_i != x_end && y_i != y_end) {
+    if (std::get<0>(*x_i) < std::get<0>(*y_i)) {
+      if (tag_names_.count(std::get<0>(*x_i)) == 0) return true;
+      ++x_i;
+    } else if (std::get<0>(*x_i) > std::get<0>(*y_i)) {
+      if (tag_names_.count(std::get<0>(*y_i)) == 0) return false;
+      ++y_i;
+    } else {
+      if (tag_names_.count(std::get<0>(*x_i)) == 0) {
+        if (metric_value::before(std::get<1>(*x_i), std::get<1>(*y_i)))
+          return true;
+        else if (metric_value::before(std::get<1>(*y_i), std::get<1>(*x_i)))
+          return false;
+      }
+
+      ++x_i;
+      ++y_i;
+    }
+  }
+
+  while (x_i != x_end) {
+    if (tag_names_.count(std::get<0>(*x_i)) == 0) return false;
+    ++x_i;
+  }
+
+  while (y_i != y_end) {
+    if (tag_names_.count(std::get<0>(*y_i)) == 0) return true;
+    ++y_i;
+  }
+
+  return false;
+}
+
+tags without_match_clause::reduce(const tags& x, const tags& y) const {
+  tags::map_type result;
+  std::copy_if(
+      x.begin(), x.end(),
+      std::inserter(result, result.end()),
+      [this](const auto& kv) {
+        return tag_names_.count(std::get<0>(kv)) == 0;
+      });
+  return tags(std::move(result));
+}
+
+
+}} /* namespace monsoon::expressions */
