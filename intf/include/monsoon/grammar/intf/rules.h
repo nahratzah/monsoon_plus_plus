cead304600466475d0356a3eadb0816c0918f510
Ariane van der Steldt <ariane@stack.nl>
2017-12-31 04:04:15 +0000

    Blacklist non printable ascii chars, and declare identifiers.

--- intf/include/monsoon/grammar/intf/rules.h
+++ intf/include/monsoon/grammar/intf/rules.h
@@ -63,6 +63,10 @@ struct append_esc {
 
 inline const auto string =
     x3::rule<class string_, std::string>("string");
+inline const auto identifier =
+    x3::rule<class identifier, std::string>("identifier");
+inline const auto quoted_identifier =
+    x3::rule<class quoted_identifier, std::string>("quoted identifier");
 inline const auto histogram_range =
     x3::rule<class histogram_range, ast::histogram_range_expr>("histogram range");
 inline const auto histogram =
@@ -70,6 +74,12 @@ inline const auto histogram =
 inline const auto value =
     x3::rule<class value, ast::value_expr>("value");
 
+inline const auto reject_chars_ =
+      x3::char_('\0') // Separate, since \0 is a string termination char.
+    | x3::char_(    "\x01\x02\x03\x04\x05\x06\x07"
+                "\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f"
+                "\x10\x11\x12\x13\x14\x15\x16\x17"
+                "\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f");
 inline const auto string_escape =
       x3::uint_parser<std::uint8_t, 8, 1, 3>()[append_utf8()]
     | 'x' >> x3::uint_parser<std::uint8_t, 16, 2, 2>()[append_utf8()]
@@ -79,9 +89,20 @@ inline const auto string_escape =
 inline const auto string_def = x3::lexeme[
     '"' >>
         *( '\\' >> string_escape
-         | (~x3::char_(R"(\")"))[([](auto& ctx){ x3::_val(ctx) += x3::_attr(ctx); })]
+         | (~x3::char_(R"(\")") - reject_chars_)[([](auto& ctx){ x3::_val(ctx) += x3::_attr(ctx); })]
          ) >>
     '"'
+    ];
+inline const auto quoted_identifier_def = x3::lexeme[
+    '\'' >>
+        *( '\\' >> string_escape
+         | (~x3::char_(R"(\')") - reject_chars_)[([](auto& ctx){ x3::_val(ctx) += x3::_attr(ctx); })]
+         ) >>
+    '\''
+    ];
+inline const auto identifier_def = x3::lexeme[
+    x3::char_("_abcdefghijklmnopqrstuvwxyz") >>
+    *x3::char_("_abcdefghijklmnopqrstuvwxyz0123456789")
     ];
 inline const auto histogram_range_def =
     x3::double_ >> x3::lit("..") >> x3::double_ >> x3::lit('=') >> x3::double_;
@@ -97,6 +118,8 @@ inline const auto value_def =
 
 BOOST_SPIRIT_DEFINE(
     string,
+    identifier,
+    quoted_identifier,
     histogram_range,
     histogram,
     value);
