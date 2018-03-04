f6a77db1fb335f5647931f11efd0512b82828ba0
Ariane van der Steldt <ariane@stack.nl>
2018-03-04 04:42:46 +0000

    Add sharded cache implementation.

--- lib/misc/include/monsoon/cache/builder_build.h
+++ lib/misc/include/monsoon/cache/builder_build.h
@@ -1,9 +1,10 @@
 #ifndef MONSOON_CACHE_BUILDER_BUILD_H
 #define MONSOON_CACHE_BUILDER_BUILD_H
 
+#include <memory>
+#include <thread>
 #include <type_traits>
 #include <utility>
-#include <memory>
 #include <monsoon/cache/builder.h>
 #include <monsoon/cache/simple_cache_impl.h>
 #include <monsoon/cache/thread_safe_decorator.h>
@@ -129,6 +130,97 @@ class wrapper final
 };
 
 
+template<typename K, typename V, typename Impl, typename Hash, typename Eq, typename Alloc, typename Create>
+class sharded_wrapper final
+: public cache_intf<K, V>
+{
+ public:
+  using store_type = typename Impl::store_type;
+  static constexpr std::size_t hash_multiplier =
+      (sizeof(std::size_t) <= 4
+       ? 0x1001000fU // 257 * 1044751
+       : 0x100010000001000fULL); // 3 * 3 * 18311 * 6996032116657
+
+  sharded_wrapper(const sharded_wrapper&) = delete;
+  sharded_wrapper(sharded_wrapper&&) = delete;
+  sharded_wrapper& operator=(const sharded_wrapper&) = delete;
+  sharded_wrapper& operator=(sharded_wrapper&&) = delete;
+
+  template<typename CreateArg>
+  sharded_wrapper(const cache_builder<K, V, Hash, Eq, Alloc>& b,
+      unsigned int shards,
+      CreateArg&& create)
+  : hash(b.hash()),
+    eq(b.equality()),
+    create(std::forward<CreateArg>(create)),
+    alloc_(b.allocator())
+  {
+    assert(shards > 1);
+    using traits = typename std::allocator_traits<Alloc>::template rebind_traits<Impl>;
+
+    traits::allocate(alloc_, shards);
+    try {
+      while (num_shards_ < shards) {
+        traits::construct(alloc_, shards_ + num_shards_, b);
+        ++num_shards_;
+      }
+    } catch (...) {
+      while (num_shards_ > 0) {
+        --num_shards_;
+        traits::destroy(alloc_, shards_ + num_shards_);
+      }
+      traits::deallocate(alloc_, shards_, shards);
+      throw;
+    }
+  }
+
+  ~sharded_wrapper() noexcept {
+    using traits = typename std::allocator_traits<Alloc>::template rebind_traits<Impl>;
+
+    unsigned int shards = num_shards_;
+    while (num_shards_ > 0) {
+      --num_shards_;
+      traits::destroy(alloc_, shards_ + num_shards_);
+    }
+    traits::deallocate(alloc_, shards_, shards);
+  }
+
+  auto get_if_present(const K& k) const
+  -> std::shared_ptr<V> override {
+    std::size_t hash_code = hash(k);
+
+    return shards_[hash_multiplier * hash_code % num_shards_].lookup_if_present(
+        make_cache_query(
+            hash_code,
+            [this, &k](const store_type& s) { return eq(s.key, k); },
+            []() { assert(false); },
+            []() { assert(false); }));
+  }
+
+  auto get(const K& k)
+  -> std::shared_ptr<V> override {
+    std::size_t hash_code = hash(k);
+    return shards_[hash_multiplier * hash_code % num_shards_].lookup_or_create(
+        make_cache_query(
+            hash_code,
+            [this, &k](const store_type& s) { return eq(s.key, k); },
+            [this, &k]() { return std::make_tuple(k); },
+            [this, &k](auto alloc) {
+              return std::allocate_shared<V>(alloc, create(k));
+            }));
+  }
+
+  Hash hash;
+  Eq eq;
+  Create create;
+
+ private:
+  typename std::allocator_traits<Alloc>::template rebind_alloc<Impl> alloc_;
+  Impl* shards_ = nullptr;
+  unsigned int num_shards_ = 0;
+};
+
+
 } /* namespace monsoon::cache::builder_detail */
 
 
@@ -138,15 +230,27 @@ auto cache_builder<K, V, Hash, Eq, Alloc>::build(Fn&& fn) const
 -> cache<K, V> {
   using namespace builder_detail;
 
+  const unsigned int shards = (!thread_safe()
+      ? 1u
+      : (concurrency() == 0u ? std::max(1u, std::thread::hardware_concurrency()) : concurrency()));
+
   auto builder_impl =
       apply_key_type(*this,
           apply_thread_safe(*this,
-              [this, &fn](auto decorators) -> std::shared_ptr<cache_intf<K, V>> {
+              [this, &fn, shards](auto decorators) -> std::shared_ptr<cache_intf<K, V>> {
                 using basic_type = typename decltype(decorators)::template cache_type<V, Alloc>;
                 using wrapper_type = wrapper<K, V, basic_type, Hash, Eq, Fn>;
-                return std::allocate_shared<wrapper_type>(
-                    allocator(),
-                    *this, std::forward<Fn>(fn));
+                using sharded_wrapper_type = sharded_wrapper<K, V, basic_type, Hash, Eq, Alloc, Fn>;
+
+                if (shards != 1u) {
+                  return std::allocate_shared<sharded_wrapper_type>(
+                      allocator(),
+                      *this, shards, std::forward<Fn>(fn));
+                } else {
+                  return std::allocate_shared<wrapper_type>(
+                      allocator(),
+                      *this, std::forward<Fn>(fn));
+                }
               }));
 
   return cache<K, V>(builder_impl(cache_decorator_set<>()));
