808948cd11064d7f37e91f00868d701ade998c8a
Ariane van der Steldt <ariane@stack.nl>
2018-03-07 23:00:17 +0000

    Add tests for metric name.

---
+++ intf/tests/metric_name.cc
@@ -0,0 +1,91 @@
+#include <monsoon/metric_name.h>
+#include <monsoon/objpipe/of.h>
+#include "hacks.h"
+#include "UnitTest++/UnitTest++.h"
+
+using namespace monsoon;
+using namespace std::string_view_literals;
+using namespace std::string_literals;
+
+TEST(constructor) {
+  CHECK_EQUAL(
+      metric_name::path_type(),
+      metric_name().get_path());
+
+  CHECK_EQUAL(
+      metric_name::path_type({ metric_name::path_type::value_type("foo"), metric_name::path_type::value_type("bar") }),
+      metric_name({ "foo", "bar" }).get_path());
+
+  CHECK_EQUAL(
+      metric_name::path_type({ metric_name::path_type::value_type("foo"), metric_name::path_type::value_type("bar") }),
+      metric_name({ "foo"s, "bar"s }).get_path());
+
+  std::vector<std::string> sv = { "foo", "bar", "baz" };
+  CHECK_EQUAL(
+      metric_name::path_type({
+          metric_name::path_type::value_type("foo"),
+          metric_name::path_type::value_type("bar"),
+          metric_name::path_type::value_type("baz")
+      }),
+      metric_name(sv.begin(), sv.end()).get_path());
+
+  auto input_iter_list = objpipe::of("foo"sv, "bar"sv, "baz"sv);
+  CHECK_EQUAL(
+      metric_name::path_type({
+          metric_name::path_type::value_type("foo"),
+          metric_name::path_type::value_type("bar"),
+          metric_name::path_type::value_type("baz")
+      }),
+      metric_name(sv.begin(), sv.end()).get_path());
+}
+
+TEST(config_string) {
+  CHECK_EQUAL(
+      "",
+      metric_name().config_string());
+
+  CHECK_EQUAL(
+      "foo.bar.baz",
+      metric_name({ "foo", "bar", "baz" }).config_string());
+
+  CHECK_EQUAL(
+      R"('\U0001fe00'.'\b'.'\v')",
+      metric_name({ u8"\U0001fe00", "\b", "\v" }).config_string());
+}
+
+TEST(equality) {
+  CHECK_EQUAL(
+      true,
+      metric_name({ "foo", "bar" }) == metric_name({ "foo"s, "bar"s }));
+  CHECK_EQUAL(
+      false,
+      metric_name({ "foo", "bar" }) != metric_name({ "foo"s, "bar"s }));
+
+  CHECK_EQUAL(
+      false,
+      metric_name({ "foo", "bar" }) == metric_name({ "bar"s }));
+  CHECK_EQUAL(
+      true,
+      metric_name({ "foo", "bar" }) != metric_name({ "bar"s }));
+
+  CHECK_EQUAL(
+      true,
+      metric_name({ "foo" }) < metric_name({ "foo", "b" }));
+  CHECK_EQUAL(
+      false,
+      metric_name({ "foo" }) < metric_name({ "foo" }));
+  CHECK_EQUAL(
+      false,
+      metric_name({ "foo", "b" }) < metric_name({ "foo" }));
+
+  CHECK_EQUAL(
+      true,
+      metric_name({ "X" }) < metric_name({ "Y" }));
+  CHECK_EQUAL(
+      false,
+      metric_name({ "Y" }) < metric_name({ "X" }));
+}
+
+int main() {
+  return UnitTest::RunAllTests();
+}
