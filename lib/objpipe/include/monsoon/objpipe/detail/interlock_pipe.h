54086ac2eb9da47c07523ed89fd67e482c23a047
Ariane van der Steldt <ariane@stack.nl>
2018-03-23 18:34:36 +0000

    Instead of dropping the exception pointer, just return the state of the objpipe prior to pushing the exception.

--- lib/objpipe/include/monsoon/objpipe/detail/interlock_pipe.h
+++ lib/objpipe/include/monsoon/objpipe/detail/interlock_pipe.h
@@ -365,16 +365,10 @@ class interlock_writer {
   }
 
   auto push_exception(std::exception_ptr exptr)
-  -> void {
+  -> objpipe_errc {
     assert(ptr_ != nullptr);
     if (exptr == nullptr) throw std::invalid_argument("nullptr exception_ptr");
-    objpipe_errc e = ptr_->publish_exception(std::move(exptr));
-
-    // We don't throw if objpipe went bad, which would indicate another
-    // exception having been pushed.
-    // Instead, we silently drop the exception.
-    if (e != objpipe_errc::success && e != objpipe_errc::bad)
-      throw objpipe_error(e);
+    return ptr_->publish_exception(std::move(exptr));
   }
 
   auto push_exception(std::exception_ptr exptr, objpipe_errc& e)
