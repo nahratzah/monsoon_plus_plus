20051bc125389faa3db24f8266af3d1d3d5e53f1
Ariane van der Steldt <ariane@stack.nl>
2018-03-26 00:11:59 +0100

    Add notion of a task.

--- lib/objpipe/include/monsoon/objpipe/detail/push_op.h
+++ lib/objpipe/include/monsoon/objpipe/detail/push_op.h
@@ -717,6 +717,46 @@ class promise_reducer {
 };
 
 
+template<typename Fn, typename... Args>
+class task {
+ public:
+  template<typename FnArg, typename... ArgsArg, typename = std::enable_if_t<sizeof...(ArgsArg) == sizeof...(Args)>>
+  explicit task(FnArg&& fn, ArgsArg&&... args)
+  noexcept(std::conjunction_v<
+      std::is_nothrow_constructible<Fn, FnArg>,
+      std::is_nothrow_constructible<Args, ArgsArg>...>)
+  : fn_(std::forward<FnArg>(fn)),
+    args_(std::forward<ArgsArg>(args)...)
+  {}
+
+  task(task&& rhs)
+  noexcept(std::conjunction_v<
+      std::is_nothrow_move_constructible<Fn>,
+      std::is_nothrow_move_constructible<Args>...>)
+  : fn_(std::move(rhs.fn_)),
+    args_(std::move(rhs.args_))
+  {}
+
+  auto operator()()
+  noexcept(noexcept(std::apply(std::declval<Fn>(), std::declval<std::tuple<Args...>>())))
+  -> auto {
+    return std::apply(std::move(fn_), std::move(args_));
+  }
+
+ private:
+  Fn fn_;
+  std::tuple<Args...> args_;
+};
+
+template<typename Fn, typename... Args>
+auto make_task(Fn&& fn, Args&&... args)
+-> task<std::decay_t<Fn>, std::remove_cv_t<std::remove_reference_t<Args>>...> {
+  return task<std::decay_t<Fn>, std::remove_cv_t<std::remove_reference_t<Args>>...>(
+      std::forward<Fn>(fn),
+      std::forward<Args>(args)...);
+}
+
+
 } /* namespace monsoon::objpipe::detail::adapt_async */
 
 
@@ -837,35 +877,36 @@ auto ioc_push(Source&& src, [[maybe_unused]] singlethread_push push_tag, Accepto
       && !std::is_const_v<Source&&>,
       "Source must be passed by (non-const) rvalue reference.");
   std::thread thr{
-      [](Source&& src, std::decay_t<Acceptor>&& acceptor) {
-        try {
-          for (;;) {
-            auto tx = adapt::raw_pull(src);
-            using value_type = typename decltype(tx)::value_type;
+    adapt_async::make_task(
+        [](Source&& src, std::decay_t<Acceptor>&& acceptor) {
+          try {
+            for (;;) {
+              auto tx = adapt::raw_pull(src);
+              using value_type = typename decltype(tx)::value_type;
 
-            if (tx.has_value()) {
-              objpipe_errc e;
-              if constexpr(is_invocable_v<std::decay_t<Acceptor>&, typename decltype(tx)::type>)
-                e = std::invoke(acceptor, std::move(tx).value());
-              else if constexpr(is_invocable_v<std::decay_t<Acceptor>&, value_type> && std::is_const_v<typename decltype(tx)::type>)
-                e = std::invoke(acceptor, std::move(tx).by_value().value());
-              else
-                e = std::invoke(acceptor, tx.ref());
+              if (tx.has_value()) {
+                objpipe_errc e;
+                if constexpr(is_invocable_v<std::decay_t<Acceptor>&, typename decltype(tx)::type>)
+                  e = std::invoke(acceptor, std::move(tx).value());
+                else if constexpr(is_invocable_v<std::decay_t<Acceptor>&, value_type> && std::is_const_v<typename decltype(tx)::type>)
+                  e = std::invoke(acceptor, std::move(tx).by_value().value());
+                else
+                  e = std::invoke(acceptor, tx.ref());
 
-              if (e != objpipe_errc::success)
+                if (e != objpipe_errc::success)
+                  break;
+              } else if (tx.errc() == objpipe_errc::closed) {
                 break;
-            } else if (tx.errc() == objpipe_errc::closed) {
-              break;
-            } else {
-              throw objpipe_error(tx.errc());
+              } else {
+                throw objpipe_error(tx.errc());
+              }
             }
+          } catch (...) {
+            acceptor.push_exception(std::current_exception());
           }
-        } catch (...) {
-          acceptor.push_exception(std::current_exception());
-        }
-      },
-      std::move(src),
-      std::forward<Acceptor>(acceptor)
+        },
+        std::move(src),
+        std::forward<Acceptor>(acceptor))
   };
 
   thr.detach();
