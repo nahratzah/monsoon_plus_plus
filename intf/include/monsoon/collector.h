015f7150af3f6ea3f24330be07f3dba9c8d7c4f6
Ariane van der Steldt <ariane@stack.nl>
2018-04-04 22:19:19 +0100

    Create specialization for synchronous collector.

--- intf/include/monsoon/collector.h
+++ intf/include/monsoon/collector.h
@@ -13,7 +13,9 @@
 #include <monsoon/time_point.h>
 #include <objpipe/reader.h>
 #include <tuple>
-#include <unordered_set>
+#include <functional>
+#include <memory>
+#include <set>
 #include <vector>
 
 namespace monsoon {
@@ -26,7 +28,7 @@ namespace monsoon {
  */
 class monsoon_intf_export_ collector {
  public:
-  using known_names_set = std::unordered_set<std::tuple<group_name, metric_name>>;
+  using known_names_set = std::set<std::tuple<group_name, metric_name>>;
   using unknown_names_set = std::vector<std::tuple<path_matcher, tag_matcher, path_matcher>>;
 
   struct names_set {
@@ -63,7 +65,39 @@ class monsoon_intf_export_ collector {
    *
    * The yielded metrics must match the constraint returned by provides().
    */
-  virtual auto run(objpipe::reader<time_point>) -> objpipe::reader<collection> = 0;
+  virtual auto run(objpipe::reader<time_point> tp_pipe) -> objpipe::reader<collection> = 0;
+};
+
+
+/**
+ * \brief Synchronous collector.
+ * \ingroup intf
+ * \details
+ * This is a collector that retrieves metrics immediately.
+ *
+ * Immediately means there's no waiting involved, like for instance
+ * it does not require a network connection.
+ *
+ * Mainly used for system metrics and internal metrics.
+ */
+class monsoon_intf_export_ sync_collector
+: public collector,
+  public std::enable_shared_from_this<sync_collector>
+{
+ public:
+  sync_collector(known_names_set names) noexcept
+  : names_(std::move(names))
+  {}
+
+  ~sync_collector() noexcept override;
+
+  auto provides() const -> names_set override final;
+  auto run(objpipe::reader<time_point> tp_pipe) -> objpipe::reader<collection> override final;
+
+ private:
+  virtual auto do_collect() -> std::vector<collection_element> = 0;
+
+  known_names_set names_;
 };
 
 
--- intf/src/collector.cc
+++ intf/src/collector.cc
@@ -6,4 +6,22 @@ namespace monsoon {
 collector::~collector() noexcept {}
 
 
+sync_collector::~sync_collector() noexcept {}
+
+auto sync_collector::provides() const
+-> names_set {
+  return { names_, {} };
+}
+
+auto sync_collector::run(objpipe::reader<time_point> tp_pipe)
+-> objpipe::reader<collection> {
+  auto self = this->shared_from_this();
+  return std::move(tp_pipe)
+      .transform(
+          [self](time_point tp) -> collection {
+            return { tp, self->do_collect(), true };
+          });
+}
+
+
 } /* namespace monsoon */
