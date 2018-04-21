04f2c22fdfba0b81dda144439d9f74af84872d15
Ariane van der Steldt <ariane@stack.nl>
2018-04-21 15:24:48 +0100

    Create a tiny history that simply prints values to stderr, while being empty.

---
+++ history/include/monsoon/history/print_history.h
@@ -0,0 +1,38 @@
+#ifndef MONSOON_HISTORY_PRINT_HISTORY_H
+#define MONSOON_HISTORY_PRINT_HISTORY_H
+
+#include <monsoon/history/history_export_.h>
+#include <monsoon/history/collect_history.h>
+
+namespace monsoon {
+
+
+///\brief A simple history that prints out all its received values.
+class monsoon_history_export_ print_history
+: public collect_history
+{
+ public:
+  ~print_history() noexcept override;
+
+  void push_back(const time_series&) override;
+
+  auto time() const -> std::tuple<time_point, time_point> override;
+
+  auto emit(
+      time_range tr,
+      path_matcher group_filter,
+      tag_matcher group_tag_filter,
+      path_matcher metric_filter,
+      time_point::duration slack) const
+      -> objpipe::reader<emit_type> override;
+
+  auto emit_time(
+      time_range tr,
+      time_point::duration slack) const
+      -> objpipe::reader<time_point> override;
+};
+
+
+} /* namespace monsoon */
+
+#endif /* MONSOON_HISTORY_PRINT_HISTORY_H */
---
+++ history/src/print_history.cc
@@ -0,0 +1,61 @@
+#include <monsoon/history/print_history.h>
+#include <monsoon/time_point.h>
+#include <monsoon/group_name.h>
+#include <monsoon/metric_name.h>
+#include <monsoon/metric_value.h>
+#include <iostream>
+#include <objpipe/of.h>
+
+namespace monsoon {
+
+
+print_history::~print_history() noexcept {}
+
+void print_history::push_back(const time_series& ts) {
+  const time_point tp = ts.get_time();
+
+  for (const time_series_value& tsv : ts.get_data()) {
+    const group_name& group = tsv.get_name();
+
+    for (const auto& metric_pair : tsv.get_metrics()) {
+      const metric_name& metric = metric_pair.first;
+      const metric_value& value = metric_pair.second;
+
+      std::cerr
+          << tp
+          << " "
+          << group
+          << "::"
+          << metric
+          << " = "
+          << value
+          << std::endl;
+    }
+  }
+}
+
+auto print_history::time() const
+-> std::tuple<time_point, time_point> {
+  time_point tp = time_point::now();
+  return { tp, tp };
+}
+
+auto print_history::emit(
+    time_range tr,
+    path_matcher group_filter,
+    tag_matcher group_tag_filter,
+    path_matcher metric_filter,
+    time_point::duration slack) const
+-> objpipe::reader<emit_type> {
+  return objpipe::of<emit_type>();
+}
+
+auto print_history::emit_time(
+    time_range tr,
+    time_point::duration slack) const
+-> objpipe::reader<time_point> {
+  return objpipe::of<time_point>();
+}
+
+
+} /* namespace monsoon */
