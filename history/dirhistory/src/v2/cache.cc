c6e64a81eace032d284fa8d7a2828e08b0f04b76
Ariane van der Steldt <ariane@stack.nl>
2018-04-23 03:46:00 +0100

    Add instrumentation for dirhistory dynamics cache.

--- history/dirhistory/src/v2/cache.cc
+++ history/dirhistory/src/v2/cache.cc
@@ -1,14 +1,23 @@
 #include "cache.h"
 #include <monsoon/cache/impl.h>
+#include <monsoon/history/instrumentation.h>
+#include <instrumentation/group.h>
 
 namespace monsoon::history::v2 {
+namespace {
 
+
+instrumentation::group&& cache_grp = instrumentation::make_group("tsdata", history_instrumentation);
+
+
+} /* namespace monsoon::history::v2::<unnamed> */
 
 auto get_dynamics_cache_() -> cache_type& {
   static cache_type impl = cache_type::builder()
       .access_expire(std::chrono::minutes(15))
       .async(true)
       .max_memory(256 * 1024 * 1024)
+      .stats("cache", cache_grp)
       .build(dynamics_cache_create());
   return impl;
 }
