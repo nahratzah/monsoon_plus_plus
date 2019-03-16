b59e45bedd8496563c3917f486a0a084cbbbc9b2
Ariane van der Steldt <ariane@stack.nl>
2019-03-16 13:43:32 +0000

    Fix: removed function that was actually important.

--- history/src/collect_history.cc
+++ history/src/collect_history.cc
@@ -6,5 +6,9 @@ namespace monsoon {
 
 collect_history::~collect_history() noexcept = default;
 
+auto collect_history::push_back(const metric_emit& m) -> void {
+  if (!std::get<1>(m).empty()) do_push_back_(m);
+}
+
 
 } /* namespace monsoon */
