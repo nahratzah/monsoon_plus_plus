36801940f441df881acdba417028ad7631c00d11
Ariane van der Steldt <ariane@stack.nl>
2019-05-25 17:03:47 +0100

    Document txfile and add create operation.

--- history/dirhistory/include/monsoon/history/dir/io/txfile.h
+++ history/dirhistory/include/monsoon/history/dir/io/txfile.h
@@ -10,6 +10,35 @@
 namespace monsoon::history::io {
 
 
+/**
+ * \brief A transactional file.
+ * \details
+ * A transactional file works by creating the illusion of a file that is
+ * modified by atomically committable transactions.
+ *
+ * The model of the transaction isolation implemented by this file is the serializable isolation:
+ * - the transaction won't be able to read data that is not committed
+ * - the transaction, when re-reading data previously read, will read the same data and won't see commits from other transactions
+ *
+ * Furthermore, this class guarantees that data that was successfully committed
+ * will be stored on disk even in the event of a crash of the binary or the system.
+ *
+ * The txfile achieves this by using a WAL (Write Ahead Log) to record operations.
+ * A designated region holds this WAL.
+ *
+ * Operations on the logical view of the file pretend the WAL doesn't exist,
+ * by translating all offset for file operations to the end of the WAL.
+ * For instance: a read or write at offset 0 would correspond to the first byte
+ * after the WAL in the actual file.
+ *
+ * The number of transactions and the size of modifications by them are limited
+ * by the size of the WAL.
+ * If the WAL region fills up and can't be compacted, any transactions in flight
+ * may error out until enough space becomes available.
+ *
+ * The transactional file has a concept of file size.
+ * Only one transaction at a time can change the file size.
+ */
 class txfile {
   public:
   class transaction;
@@ -17,15 +46,46 @@ class txfile {
   txfile(const txfile&) = delete;
   txfile& operator=(const txfile&) = delete;
 
+  ///\brief Create a new txfile object that does not point at a file.
   txfile() {}
+
+  /**
+   * \brief Open an existing txfile.
+   * \details
+   * \param[in] fd The file descriptor of the file.
+   * \param[in] off The offset at which the WAL is found.
+   * \param[in] len The length in bytes of the WAL.
+   */
   txfile(monsoon::io::fd&& fd, monsoon::io::fd::offset_type off, monsoon::io::fd::size_type len);
 
+  private:
+  struct create_tag {};
+  txfile(create_tag tag, monsoon::io::fd&& fd, monsoon::io::fd::offset_type off, monsoon::io::fd::size_type len);
+
+  public:
   txfile(txfile&&) noexcept = default;
   txfile& operator=(txfile&&) noexcept = default;
 
   ~txfile() noexcept;
 
+  auto create(monsoon::io::fd&& fd, monsoon::io::fd::offset_type off, monsoon::io::fd::size_type len) -> txfile;
+
+  /**
+   * \brief Start a new transaction.
+   * \details
+   * Start a new transaction.
+   * If the \p read_only parameter is omitted, the code defaults to a read-only transaction.
+   * \param[in] read_only If set, the transaction shall be a read-only transaction.
+   * \return A new transaction.
+   */
   auto begin(bool read_only) -> transaction;
+  /**
+   * \brief Start a new read-only transaction.
+   * \details
+   * Start a new transaction.
+   * Because the txfile is const, only a read-only transaction can be started.
+   * \return A new read-only transaction.
+   */
   auto begin() const -> transaction;
 
   private:
@@ -35,16 +95,24 @@ class txfile {
 };
 
 
+/**
+ * \brief Transaction object for transactional files.
+ * \details
+ * This object is used to interact with the contents of a txfile instance.
+ */
 class txfile::transaction {
   friend txfile;
 
   public:
+  ///\brief The offset type for the file modeled by the transaction.
   using offset_type = monsoon::io::fd::offset_type;
+  ///\brief The size type for the file modeled by the transaction.
   using size_type = monsoon::io::fd::size_type;
 
   transaction(const transaction&) = delete;
   transaction& operator=(const transaction&) = delete;
 
+  ///\brief Transactions are swappable.
   friend void swap(transaction& x, transaction& y) noexcept {
     using std::swap;
     swap(x.read_only_, y.read_only_);
@@ -52,14 +120,28 @@ class txfile::transaction {
     swap(x.seq_, y.seq_);
   }
 
+  /**
+   * \brief Create an uninitialized, invalid transaction.
+   * \details
+   * The transaction is not started and can not have operations applied.
+   */
   transaction() = default;
 
+  /**
+   * \brief Move construct a transaction.
+   */
   transaction(transaction&& x) noexcept
   : read_only_(x.read_only_),
     owner_(std::exchange(x.owner_, nullptr)),
     seq_(std::move(x.seq_))
   {}
 
+  /**
+   * \brief Move assign a transaction.
+   * \details
+   * If the current object has an in-progress transaction,
+   * that transaction will be rolled back.
+   */
   transaction& operator=(transaction&& x) noexcept {
     if (*this) rollback();
     read_only_ = x.read_only_;
@@ -69,6 +151,11 @@ class txfile::transaction {
   }
 
   private:
+  /**
+   * \brief Internal transaction constructor.
+   * \details
+   * This constructor is used by txfile to begin a new transaction.
+   */
   explicit transaction(bool read_only, txfile& owner)
   : read_only_(read_only),
     owner_(&owner),
@@ -76,19 +163,47 @@ class txfile::transaction {
   {}
 
   public:
+  /**
+   * \brief Cancel the transaction if it is in progress.
+   */
   ~transaction() noexcept {
     if (*this) rollback();
   }
 
+  /**
+   * \brief Test if this transaction is valid.
+   * \details
+   * A valid transaction can execute operations.
+   */
   explicit operator bool() const noexcept {
     return owner_ != nullptr;
   }
 
+  /**
+   * \brief Test if the transaction is invalid.
+   * \details
+   * An invalid transaction is one that has been committed or rolled back.
+   */
   auto operator!() const noexcept -> bool {
     return !this->operator bool();
   }
 
+  /**
+   * \brief Commit this transaction.
+   * \details
+   * Changes made by this transaction will be visible to transactions started
+   * the commit completes.
+   *
+   * Transactions started before the commit completes won't observe changes
+   * made by this transaction.
+   */
   void commit();
+  /**
+   * \brief Cancel this transaction.
+   * \details
+   * None of the changes made by this transaction will be visible to other
+   * transactions.
+   */
   void rollback() noexcept;
 
   auto write_at(offset_type off, const void* buf, std::size_t nbytes) -> std::size_t;
--- history/dirhistory/src/io/txfile.cc
+++ history/dirhistory/src/io/txfile.cc
@@ -8,7 +8,16 @@ txfile::txfile(monsoon::io::fd&& fd, monsoon::io::fd::offset_type off, monsoon::
   wal_(fd_, off, len)
 {}
 
+txfile::txfile([[maybe_unused]] create_tag tag, monsoon::io::fd&& fd, monsoon::io::fd::offset_type off, monsoon::io::fd::size_type len)
+: fd_(std::move(fd)),
+  wal_(wal_region::create(fd_, off, len))
+{}
+
 txfile::~txfile() noexcept = default;
+
+auto txfile::create(monsoon::io::fd&& fd, monsoon::io::fd::offset_type off, monsoon::io::fd::size_type len) -> txfile {
+  return txfile(create_tag(), std::move(fd), off, len);
+}
 
 
 } /* namespace monsoon::history::io */
